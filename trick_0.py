da3a9cb81275616652d200c577e19265c8a26b1d
chubbyX <91040760+SeaBreezing@users.noreply.github.com>
2022-07-28 22:58:52 +0800

    Create trick_0.py

---
+++ trick_0.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+class EMA():
+    def __init__(self, model, decay):
+        self.model = model
+        self.decay = decay
+        self.shadow = {}
+        self.backup = {}
+
+    def register(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                self.shadow[name] = param.data.clone()
+
+    def update(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name]
+                self.shadow[name] = new_average.clone()
+
+    def apply_shadow(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                self.backup[name] = param.data
+                param.data = self.shadow[name]
+
+    def restore(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
+        
+class FGM:
+    def __init__(self, model: nn.Module, eps=1.):
+        self.model = (
+            model.module if hasattr(model, "module") else model
+        )
+        self.eps = eps
+        self.backup = {}
+
+    # only attack word embedding
+    def attack(self, emb_name='word_embeddings'):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+                self.backup[name] = param.data.clone()
+                norm = torch.norm(param.grad)
+                if norm and not torch.isnan(norm):
+                    r_at = self.eps * param.grad / norm
+                    param.data.add_(r_at)
+
+    def restore(self, emb_name='word_embeddings'):
+        for name, para in self.model.named_parameters():
+            if para.requires_grad and emb_name in name:
+                assert name in self.backup
+                para.data = self.backup[name]
+
+        self.backup = {}
+
