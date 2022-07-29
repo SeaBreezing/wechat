aab1cb44a100628c5a5eaecd1926caad1c58a307
chubbyX <91040760+SeaBreezing@users.noreply.github.com>
2022-07-29 10:21:22 +0800

    Update trick_0.py

--- trick_0.py
+++ trick_0.py
@@ -1,25 +1,30 @@
 import torch
 import torch.nn as nn
+#EMA对所有name和param操作,FGA只对word_embedding操作
+#EMA不更新训练过程中的参数,只是将最后几步存起来，FGA训练过程中步步更新
 
 class EMA():
     def __init__(self, model, decay):
         self.model = model
         self.decay = decay
-        self.shadow = {}
+        self.shadow = {} # 初始化一个字典
         self.backup = {}
-
+        
+    # 完成step1
     def register(self):
         for name, param in self.model.named_parameters():
             if param.requires_grad:
                 self.shadow[name] = param.data.clone()
-
+    
+    #step2的后半步，注意没用更新参数的操作
     def update(self):
         for name, param in self.model.named_parameters():
             if param.requires_grad:
                 assert name in self.shadow
-                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name]
-                self.shadow[name] = new_average.clone()
+                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name] # decay就是beta
+                self.shadow[name] = new_average.clone() # 注意是self，共享的
 
+    # step3的后半句，注意这个函数用于validate和test,注意EMA的参数并不会用于训练时的参数更新
     def apply_shadow(self):
         for name, param in self.model.named_parameters():
             if param.requires_grad:
@@ -27,6 +32,7 @@ class EMA():
                 self.backup[name] = param.data
                 param.data = self.shadow[name]
 
+    # step3
     def restore(self):
         for name, param in self.model.named_parameters():
             if param.requires_grad:
@@ -42,7 +48,7 @@ class FGM:
         self.eps = eps
         self.backup = {}
 
-    # only attack word embedding
+    # only attack word embedding, 完成step2
     def attack(self, emb_name='word_embeddings'):
         for name, param in self.model.named_parameters():
             if param.requires_grad and emb_name in name:
@@ -51,12 +57,12 @@ class FGM:
                 if norm and not torch.isnan(norm):
                     r_at = self.eps * param.grad / norm
                     param.data.add_(r_at)
-
+    # 完成step
     def restore(self, emb_name='word_embeddings'):
         for name, para in self.model.named_parameters():
             if para.requires_grad and emb_name in name:
                 assert name in self.backup
-                para.data = self.backup[name]
+                para.data = self.backup[name] # 
 
         self.backup = {}
 
